7a52b4eb8bb496d5d4636fcacb09f5eb39420d72
Hsu Myat Moe <hsumyatm7308@gmail.com>
2024-01-23 01:13:00 +0630

    jan 23

---
+++ homemanagement/resources/views/layouts/adminheader.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Dashboard UI</title>
+  <link href="https://assets.codepen.io/344846/style.css" rel="stylesheet">
+  <link rel="stylesheet" href="{{asset('./style.css')}}">
+
+</head>
+
