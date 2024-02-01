57502f6d80d0396c98c592d853ea6d10182f0316
Hsu Myat Moe <hsumyatm7308@gmail.com>
2024-02-01 13:49:59 +0630

    feb 1

--- homemanagement/resources/views/layouts/adminheader.blade.php
+++ homemanagement/resources/views/layouts/adminheader.blade.php
@@ -7,11 +7,9 @@
 	<link href="{{asset('assets/img/fav/favicon.png')}}" rel="icon" type="image/png" sizes="16x16" />
 
 	<!-- bootstrap css1 js1 -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
-		integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    
-    <!-- Tailwind CSS -->
-    <link href="https://cdn.tailwindcss.com" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+
+
 
 
 	<!-- fontawesome css1 -->
